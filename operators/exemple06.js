c6eefb0eaf4f387e0c8f5f1c450325aa8fdfc79f
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-11-20 00:37:51 -0300

    feat: ✨ generict function to create pipeple operator

---
+++ operators/exemple06.js
@@ -0,0 +1,47 @@
+const { from, Observable } = require('rxjs');
+
+function createPipeapleOperator(nextFn) {
+    return function (source) {
+        return new Observable(subscriber => {
+            source.subscribe({
+               next(value) {
+                 nextFn(subscriber, value)
+               }
+            })
+        })
+    }
+}
+
+function firstElement() {
+    return createPipeapleOperator((subscriber, value) => {
+        subscriber.next(value)
+        subscriber.complete()
+    })
+}
+
+function lastElement() {
+    return function (source) {
+        return new Observable(subscriber => {
+            let last;
+            source.subscribe({
+                next(value) {
+                    last = value
+                },
+                complete() {
+                    subscriber.next(last)
+                    subscriber.complete()
+                }           
+            })
+        })
+    }
+}
+
+
+from([1, 2, 3, 4, 5])
+    .pipe(firstElement())
+    .subscribe(console.log);
+
+
+from([1, 2, 3, 4, 5])
+    .pipe(lastElement())
+    .subscribe(console.log);
